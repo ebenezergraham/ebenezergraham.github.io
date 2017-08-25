717602265bbaf2aedbb3c9d662a7e5a8e2ed02ae
Ebenezer Graham <egraham15@alustudent.com>
2017-08-26 03:11:11 +0400

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -2,34 +2,31 @@
 window.onload = function(){
 var duration = 300;
 var timeUp = false;
-var display = document.getElementById('timer');
+var display = document.getElementById('clock-countdown');
 startTimer(duration, display,timeUp);
 }
-// assignValue(){
-// var min = document.getElementById('min').value;
-// var sec = document.getElementById('sec').value;
-// var duration = (min*60)+sec;
-// var timeUp = false;
-// var display = document.getElementById('timer');
-// startTimer(duration, display,timeUp);
-// }
 
 function startTimer(duration, display, timeUp) {
-    var counter = duration,min, sec;
+    var counter = duration,days, hours, min, sec;
     setInterval(function () {
+	days = parseInt(counter / 60, 10);
+	hours = parseInt(counter / 60, 10);
         min = parseInt(counter / 60, 10);
         sec = parseInt(counter % 60, 10);
-        sec = sec < 10 ? '0' + sec : sec;
+	days = days < 10 ? '0' + days : days;    
+	hours = hours < 10 ? '0' + hours : hours;
+        min = min < 10 ? '0' + min : min;
+	sec = sec < 10 ? '0' + sec : sec;
         if (min === 0 && sec == 00) {
             timeUp = true;
             if (timeUp) {
-				display.innerHTML="Time is up";
+		display.innerHTML="Time is up";
             }
-            display.innerHTML = min + ":" + sec;
+            	display.innerHTML = days + ":" + hours + min + ":" + sec;
 
         }
         if (!timeUp) {
-            display.innerHTML = min + ":" + sec;
+            display.innerHTML = days + ":" + hours + min + ":" + sec;
             document.getElementById('timer').value = counter;
         }
 		
