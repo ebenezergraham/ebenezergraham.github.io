0312bb4bb35c63ce2af0ad03f382e03b7ea51818
Ebenezer Graham <egraham15@alustudent.com>
2017-08-26 03:46:06 +0400

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -10,8 +10,8 @@ function startTimer(duration, display, timeUp) {
     var counter = duration,days, hours, min, sec;
     setInterval(function () {
 	 days = parseInt(0, 10);
-	    hours = parseInt(counter % 3600, 10);
-	    min = parseInt(hours*60 % 60, 10);
+	    hours = parseInt(counter / 3600, 10);
+	    min = parseInt((hours*60) / 60, 10);
 	sec = parseInt(counter % 60, 10);
 	
 	
