0a37c8a7b9fd32bf17a3e8e2b4f8df49e8e5952a
Lum-neh Angela <lumneh.angela385@gmail.com>
2018-05-29 19:40:33 +0400

    Revert "Changed intro message to Git session"
    This reverts commit dcd8123601cbfce22797b376733230bed499b6f3.

--- assets/js/function.js
+++ assets/js/function.js
@@ -29,7 +29,7 @@ $('.owl-carousel').owlCarousel();
 $(document).ready(function(){
 $(function() {
   $("#typed").typed({
-    strings: ["Hi There!", "Welcome to the GitHub Intro Session!", "Let's get started!"],
+    strings: ["Hi There!", "Welcome to Graham's Website!", "I interned at Cim Finance", "And it was an enlightening experience"],
     typeSpeed: 100,
     backSpeed: 20,
     loop: true,
